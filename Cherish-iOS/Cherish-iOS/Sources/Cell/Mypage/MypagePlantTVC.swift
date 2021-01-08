69ce47c3e2e6c057e059e71a13d65895ca388be7
hwangji-dev <melon2228@naver.com>
2021-01-08 21:49:38 +0900

    [Feat] add tableviewBorder in mypageView

--- Cherish-iOS/Cherish-iOS/Sources/Cell/Mypage/MypagePlantTVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/Cell/Mypage/MypagePlantTVC.swift
@@ -13,17 +13,24 @@ class MypagePlantTVC: UITableViewCell {
     @IBOutlet var mypagePlantNicknameLabel: CustomLabel!
     @IBOutlet var mypagePlantTypeLabel: CustomLabel!
     @IBOutlet var mypagePlantWateringDdayLabel: UILabel!
+    @IBOutlet var dayCornerView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        makeRadiusView(CGColor(red: 247/255, green: 89/255, blue: 108/255, alpha: 1.0))
     }
-
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    func makeRadiusView(_ borderColor: CGColor){
+        dayCornerView.layer.borderWidth = 1.5
+        dayCornerView.layer.borderColor = borderColor
+        dayCornerView.layer.cornerRadius = 13
     }
     
     func setProperties(_ imageURL:String, _ nickname:String,_ plantType:String,_ wateringDay:String){
