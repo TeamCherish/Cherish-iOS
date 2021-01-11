b7196c15af609ac6731c931a253779951a8d7e50
wonsuckl <wurijang@naver.com>
2021-01-12 03:59:46 +0900

    [Chore]WateringMotion Storyboard Create

---
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/Watering/WateringMotionVC.swift
@@ -0,0 +1,17 @@
+//
+//  WateringMotionVC.swift
+//  Cherish-iOS
+//
+//  Created by 이원석 on 2021/01/12.
+//
+
+import UIKit
+
+class WateringMotionVC: UIViewController {
+
+    @IBOutlet weak var motionLabel: CustomLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
