3bbc205211325cd3a1d90e5f458b2505f7798cdf
seohyun-106 <changseohyun@gmail.com>
2021-01-15 06:48:06 +0900

    [feature] loading view time interval

--- Cherish-iOS/Cherish-iOS/Sources/ VC/AddUser/LoadingPopUpVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/AddUser/LoadingPopUpVC.swift
@@ -11,10 +11,20 @@ class LoadingPopUpVC: UIViewController {
     
     @IBOutlet weak var loadingImgView: UIImageView!
     
+    var mTimer: Timer?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadingImgView.loadGif(asset: "loading")
+        sleep(5)
+        mTimer = Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(moveToResultVC), userInfo: nil, repeats: false)
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func moveToResultVC() {
+        guard let dvc = self.storyboard?.instantiateViewController(identifier: "PlantResultVC") as? PlantResultVC else { return }
+        self.navigationController?.pushViewController(dvc, animated: true)
     }
 
 }
