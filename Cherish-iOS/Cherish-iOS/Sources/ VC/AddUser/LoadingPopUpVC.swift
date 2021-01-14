64af65b0482e343e86274e31a4db844e59bc11ff
seohyun-106 <changseohyun@gmail.com>
2021-01-15 06:25:58 +0900

    [add] loading view animation

--- Cherish-iOS/Cherish-iOS/Sources/ VC/AddUser/LoadingPopUpVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/AddUser/LoadingPopUpVC.swift
@@ -8,22 +8,13 @@
 import UIKit
 
 class LoadingPopUpVC: UIViewController {
-
+    
+    @IBOutlet weak var loadingImgView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadingImgView.loadGif(asset: "loading")
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
