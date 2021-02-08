0bfdea56b72c48f8515869facd26a182da108696
wonsuckl <wurijang@naver.com>
2021-02-08 22:49:51 +0900

    [Chore]Base Url Change

--- Cherish-iOS/Cherish-iOS/Resources/APIService/APIConstants.swift
+++ Cherish-iOS/Cherish-iOS/Resources/APIService/APIConstants.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct APIConstants {
-    static let baseURL = "http://3.35.117.232:8080/"
+    static let baseURL = "http://cherishserver.com/"
     static let wateringReviewURL = baseURL + "water" /// 물주기[연락후기]
     static let wateringDayURL = baseURL + "search/" /// 물주는 날짜 조회
     static let laterURL = baseURL + "postpone" /// 미루기
