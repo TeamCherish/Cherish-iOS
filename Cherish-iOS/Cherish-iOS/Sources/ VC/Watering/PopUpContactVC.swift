fec3c23a8fabfff866858b71876566304033e524
wonsuckl <wurijang@naver.com>
2021-02-13 22:41:35 +0900

    [Fix]Applied Better Contact Recent Keyword  Data Logic

--- Cherish-iOS/Cherish-iOS/Sources/ VC/Watering/PopUpContactVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/Watering/PopUpContactVC.swift
@@ -31,10 +31,6 @@ class PopUpContactVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
         getRecentKeyword()
     }
     
