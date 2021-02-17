fe79ba8f4b37434adf403e2d5d555bf4359489eb
hwangji-dev <melon2228@naver.com>
2021-02-17 17:19:06 +0900

    [Fix] mypageView contacts array sorting by ascending

--- Cherish-iOS/Cherish-iOS/Sources/ VC/MyPage/MypageContactVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/MyPage/MypageContactVC.swift
@@ -21,11 +21,22 @@ class MypageContactVC: UIViewController {
         mypageContactTV.separatorStyle = .none
     }
     
+    
     func setContactData(){
         if let data = UserDefaults.standard.value(forKey: "userContacts") as? Data {
             let contacts = try? PropertyListDecoder().decode([Friend].self, from: data)
 
             mypageContactArray = contacts!
+            
+            // 숫자 -> 한글 가나다순 -> 영어순으로 정렬
+            mypageContactArray = mypageContactArray.sorted(by: {$0.name.localizedStandardCompare($1.name) == .orderedAscending })
+            // 숫자로 시작하는 문자열 추출 후 저장
+            let startNumberArray = mypageContactArray.filter{$0.name.first?.isNumber == true}
+            // 문자로 시작하는 문자열만 추출해서 저장
+            mypageContactArray = mypageContactArray.filter{$0.name.first?.isNumber == false}
+            // 문자로 시작하는 문자열 뒤에 숫자로 시작하는 문자열 병합
+            mypageContactArray.append(contentsOf: startNumberArray)
+
             print(mypageContactArray)
         }
     }
