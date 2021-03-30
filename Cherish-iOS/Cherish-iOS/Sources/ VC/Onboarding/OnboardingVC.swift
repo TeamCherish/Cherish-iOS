5c684a755e155e354099240e30b84a0881706054
wonseok <wurijang@gmail.com>
2021-03-30 16:50:24 +0900

    [Chore]Onboarding작업 시작

---
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/Onboarding/OnboardingVC.swift
@@ -0,0 +1,29 @@
+//
+//  OnboardingVC.swift
+//  Cherish-iOS
+//
+//  Created by 이원석 on 2021/03/30.
+//
+
+import UIKit
+
+class OnboardingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
