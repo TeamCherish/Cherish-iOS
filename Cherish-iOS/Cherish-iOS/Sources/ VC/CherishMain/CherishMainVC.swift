badf6f207f2cfaa4d64c55cc84cfac7d5ef8f452
hwangji-dev <melon2228@naver.com>
2021-03-31 17:11:10 +0900

    [Fix] mainView resizing

--- Cherish-iOS/Cherish-iOS/Sources/ VC/CherishMain/CherishMainVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/CherishMain/CherishMainVC.swift
@@ -14,6 +14,11 @@ enum Notches : Int, CaseIterable {
 
 class CherishMainVC: OverlayContainerViewController {
     
+    // 뷰 전체 폭 길이
+    let screenWidth = UIScreen.main.bounds.size.width
+    
+    // 뷰 전체 높이 길이
+    let screenHeight = UIScreen.main.bounds.size.height
     
     override func viewDidLoad() {
         super.viewDidLoad()
