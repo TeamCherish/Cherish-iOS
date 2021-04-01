cf8a8b7fed72364e86eccf99577060ab1ab6c33f
seohyun-106 <changseohyun@gmail.com>
2021-04-01 17:13:18 +0900

    [Fix] 식물 상세 입력 auto

--- Cherish-iOS/Cherish-iOS/Sources/ VC/AddUser/InputDetailVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/AddUser/InputDetailVC.swift
@@ -141,7 +141,7 @@ class InputDetailVC: UIViewController {
                         
                         //                        NotificationCenter.default.post(name: .sendPlantResult, object: nil)
                         self.present(loadingVC, animated: false, completion: nil)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
                             self.dismiss(animated: false) {
                                 self.navigationController?.pushViewController(resultVC, animated: true)
                             }
