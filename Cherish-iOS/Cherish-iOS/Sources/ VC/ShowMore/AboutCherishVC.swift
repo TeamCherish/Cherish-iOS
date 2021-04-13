b61d9f042a607b93dfc1fd11a59bb6b6ca552467
wonseok <wurijang@gmail.com>
2021-04-13 23:24:30 +0900

    [Fix]About Cherish AutoLayout Fixed, ScrollView Delete

--- Cherish-iOS/Cherish-iOS/Sources/ VC/ShowMore/AboutCherishVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/ShowMore/AboutCherishVC.swift
@@ -9,25 +9,10 @@ import UIKit
 
 class AboutCherishVC: UIViewController {
 
-    @IBOutlet weak var forScroll: NSLayoutConstraint!
-    @IBOutlet weak var stackBottomAnchor: NSLayoutConstraint!
-    @IBOutlet weak var weareLabelLeading: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBarController?.tabBar.isHidden = true
-        setPhoneResoulution()
         // Do any additional setup after loading the view.
-    }
-    
-    func setPhoneResoulution(){
-        if UIDevice.current.isiPhoneSE2{
-            stackBottomAnchor.constant = 43
-            forScroll.constant = 50
-        }else if UIDevice.current.isiPhoneSE{
-            weareLabelLeading.constant = 15
-            stackBottomAnchor.constant = 43
-            forScroll.constant = 150
-        }
     }
     
     //MARK: - add Swipe Guesture that go back to parentVC
