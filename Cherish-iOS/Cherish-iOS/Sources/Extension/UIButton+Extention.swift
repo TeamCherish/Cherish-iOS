ec8d464da7c50781f14165b4ed5e618b1fde28c5
wonseok <wurijang@gmail.com>
2021-05-07 00:07:03 +0900

    [Chore]Extension import Foundation 제거

--- Cherish-iOS/Cherish-iOS/Sources/Extension/UIButton+Extention.swift
+++ Cherish-iOS/Cherish-iOS/Sources/Extension/UIButton+Extention.swift
@@ -5,7 +5,6 @@
 //  Created by 황지은 on 2021/01/08.
 //
 
-import Foundation
 import UIKit
 
 extension UIButton {
--- Cherish-iOS/Cherish-iOS/Sources/Extension/UIView+Extension.swift
+++ Cherish-iOS/Cherish-iOS/Sources/Extension/UIView+Extension.swift
@@ -5,7 +5,6 @@
 //  Created by 이원석 on 2021/01/03.
 //
 
-import Foundation
 import UIKit
 
 extension UIView {
