686801da0d0079f99d806c718f356dcc710909ab
hwangji-dev <melon2228@naver.com>
2021-06-13 22:37:37 +0900

    [Feat] showMoreView OpenSourceLisence backBtn status to Enabled

--- Cherish-iOS/Cherish-iOS/Sources/ VC/ShowMore/OpenSourceContentVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/ShowMore/OpenSourceContentVC.swift
@@ -38,15 +38,7 @@ class OpenSourceContentVC: UIViewController {
             print("None")
         }
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func touchUpToBackSwipe(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
     }
-    */
-
 }
