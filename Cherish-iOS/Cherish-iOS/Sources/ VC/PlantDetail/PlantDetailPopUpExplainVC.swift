c4334bfe4713372e71348d5e271161f3523f1449
wonseok <wurijang@gmail.com>
2021-08-05 22:21:01 +0900

    [refactor] 식물 상세 뷰 팝업 카드 부분 Kingfisher 적용

--- Cherish-iOS/Cherish-iOS/Sources/ VC/PlantDetail/PlantDetailPopUpExplainVC.swift
+++ Cherish-iOS/Cherish-iOS/Sources/ VC/PlantDetail/PlantDetailPopUpExplainVC.swift
@@ -2,13 +2,19 @@
 //  PlantDetailPopUpExplainVC.swift
 //  Cherish-iOS
 //
-//  Created by 황지은 on 2021/01/04.
+//  Edited by 이원석 on 2021/08/05.
 //
 
 import UIKit
+import Kingfisher
 
 class PlantDetailPopUpExplainVC: UIViewController {
-
+    var plantId:Int?
+    var scrollItem:Int?
+    var plantStepExplainArray:[PlantDetail] = []
+    var plantExplain:[PlantResponse] = []
+    private var indexOfCellBeforeDragging = 0 // 페이징 관련 index 정의 함수
+    
     @IBOutlet var popUpPlantDetailExplainView: UIView! {
         didSet {
             popUpPlantDetailExplainView.makeRounded(cornerRadius: 20.0)
@@ -16,14 +22,7 @@ class PlantDetailPopUpExplainVC: UIViewController {
     }
     @IBOutlet var plantExplainCV: UICollectionView!
     @IBOutlet var plantExplainPageControl: UIPageControl!
-    var plantId:Int?
-    var scrollItem:Int?
     
-    var plantStepExplainArray:[PlantDetail] = []
-    var plantExplain:[PlantResponse] = []
-    
-    // 페이징 관련 index 정의 함수
-    private var indexOfCellBeforeDragging = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,7 +31,7 @@ class PlantDetailPopUpExplainVC: UIViewController {
         setPageControlProperty()
     }
     
-    func makeDelegates() {
+    private func makeDelegates() {
         plantExplainCV.delegate = self
         plantExplainCV.dataSource = self
     }
@@ -58,7 +57,7 @@ class PlantDetailPopUpExplainVC: UIViewController {
         return safeIndex
     }
     
-    func setPlantDetailExplainData() {
+    private func setPlantDetailExplainData() {
         if let plantId = self.plantId {
             print(plantId)
             PlantDetailCardService.shared.inquirePlantDetailCardView(plantIdx: plantId){ [self]
@@ -66,13 +65,8 @@ class PlantDetailPopUpExplainVC: UIViewController {
                 switch networkResult {
                 case .success(let data):
                     if let detailCardData = data as? PlantDetailCardData {
-                        print("통신이 됩니닷")
                         plantExplain = detailCardData.plantResponse
-                        // 4개 카드 중 3개 정보 받는 array
-                        plantStepExplainArray = detailCardData.plantDetail
-                        
-                        print("plantExplain",plantExplain)
-                        print("plantStepExplainArray",plantStepExplainArray)
+                        plantStepExplainArray = detailCardData.plantDetail // 4개 카드 중 3개 정보 받는 array
                         plantExplainCV.reloadData()
                     }
                 case .requestErr(let msg):
@@ -84,18 +78,39 @@ class PlantDetailPopUpExplainVC: UIViewController {
                 case .networkFail:
                     print("networkFail")
                 }
-                
             }
         }
     }
     
-    func setPageControlProperty() {
+    private func setPageControlProperty() {
         plantExplainPageControl.hidesForSinglePage = true
         plantExplainPageControl.numberOfPages = 4
         plantExplainPageControl.pageIndicatorTintColor = UIColor(red: 233/255, green: 233/255, blue: 233/255, alpha: 1.0)
         plantExplainPageControl.currentPageIndicatorTintColor = .seaweed
     }
     
+    // MARK: - 식물 단계 별 이미지와 설명 에셋 세팅
+    private func setPopUpAsset(_ imageView: UIImageView, _ url: URL) {
+        let processor = DownsamplingImageProcessor(size: imageView.frame.size)
+        imageView.kf.indicatorType = .activity
+        imageView.kf.setImage(
+            with: url,
+            placeholder: nil,
+            options: [
+                .processor(processor),
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(0.5)),
+                .cacheOriginalImage
+            ]) {
+            result in
+            switch result {
+            case .success(_):
+                print("Task done")
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
     
     @IBAction func dismissPopUpExplainView(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
@@ -111,94 +126,58 @@ extension PlantDetailPopUpExplainVC : UICollectionViewDelegate, UICollectionView
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let plantExplainCell = collectionView.dequeueReusableCell(withReuseIdentifier: "PlantExplainCVC", for: indexPath) as! PlantExplainCVC
-        if plantExplain.count != 0 {
+        
+        if !plantExplain.isEmpty {
             
-            if indexPath.item == 0 {
+            switch indexPath.item {
+            case 0:
                 plantExplainCell.plantExplainTitleLabel.text = plantExplain[0].modifier
                 plantExplainCell.plantExplainSubtitleLabel.text = plantExplain[0].explanation
                 
-                let first_url = URL(string: plantExplain[0].image)
-                let first_imageData = try? Data(contentsOf: first_url!)
-                plantExplainCell.flowerMeaningImageView.image = UIImage(data: first_imageData!)
-                
-                /// 이미지 url 처리
-                DispatchQueue.global(qos: .default).async(execute: { [self]() -> Void in
-                    
-                    let url = URL(string: plantExplain[0].imageURL )
-                    let imageData = try? Data(contentsOf: url!)
-                    DispatchQueue.main.async(execute: {() -> Void in
-                        
-                        plantExplainCell.plantImageView.image = UIImage(data: imageData!)
-                    })
-                })
-                
-//                plantExplainCell.plantImageView.image = UIImage(named: "imgMinLevel3")
-            }
-            else if indexPath.item == 1 {
+                if let meaningUrl = URL(string: plantExplain[0].image) {
+                    setPopUpAsset(plantExplainCell.flowerMeaningImageView, meaningUrl)
+                }
+                if let imageUrl = URL(string: plantExplain[0].imageURL) {
+                    setPopUpAsset(plantExplainCell.plantImageView, imageUrl)
+                }
+            case 1:
                 plantExplainCell.plantExplainTitleLabel.text = "1단계"
                 plantExplainCell.plantExplainSubtitleLabel.text = plantStepExplainArray[0].plantDetailDescription
                 
-                let step1 = URL(string: plantStepExplainArray[0].image)
-                let step1_imageData = try? Data(contentsOf: step1!)
-                plantExplainCell.flowerMeaningImageView.image = UIImage(data: step1_imageData!)
-                
-                /// 이미지 url 처리
-                DispatchQueue.global(qos: .default).async(execute: { [self]() -> Void in
-                    
-                    let url = URL(string: plantStepExplainArray[0].imageURL)
-                    let imageData = try? Data(contentsOf: url!)
-                    DispatchQueue.main.async(execute: {() -> Void in
-                        
-                        plantExplainCell.plantImageView.image = UIImage(data: imageData!)
-                    })
-                })
-            }
-            else if indexPath.item == 2 {
+                if let meaningUrl = URL(string: plantStepExplainArray[0].image) {
+                    setPopUpAsset(plantExplainCell.flowerMeaningImageView, meaningUrl)
+                }
+                if let imageUrl = URL(string: plantStepExplainArray[0].imageURL) {
+                    setPopUpAsset(plantExplainCell.plantImageView, imageUrl)
+                }
+            case 2:
                 plantExplainCell.plantExplainTitleLabel.text = "2단계"
                 plantExplainCell.plantExplainSubtitleLabel.text = plantStepExplainArray[1].plantDetailDescription
                 
-                let step2 = URL(string: plantStepExplainArray[1].image)
-                let step2_imageData = try? Data(contentsOf: step2!)
-                plantExplainCell.flowerMeaningImageView.image = UIImage(data: step2_imageData!)
-                /// 이미지 url 처리
-                DispatchQueue.global(qos: .default).async(execute: { [self]() -> Void in
-                    
-                    let url = URL(string: plantStepExplainArray[1].imageURL)
-                    let imageData = try? Data(contentsOf: url!)
-                    DispatchQueue.main.async(execute: {() -> Void in
-                        
-                        plantExplainCell.plantImageView.image = UIImage(data: imageData!)
-                    })
-                })
-            }
-            else {
+                if let meaningUrl = URL(string: plantStepExplainArray[1].image) {
+                    setPopUpAsset(plantExplainCell.flowerMeaningImageView, meaningUrl)
+                }
+                if let imageUrl = URL(string: plantStepExplainArray[1].imageURL) {
+                    setPopUpAsset(plantExplainCell.plantImageView, imageUrl)
+                }
+            default:
                 plantExplainCell.plantExplainTitleLabel.text = "3단계"
                 plantExplainCell.plantExplainSubtitleLabel.text = plantStepExplainArray[1].plantDetailDescription
                 
-                let step3 = URL(string: plantStepExplainArray[2].image)
-                let step3_imageData = try? Data(contentsOf: step3!)
-                plantExplainCell.flowerMeaningImageView.image = UIImage(data: step3_imageData!)
-                /// 이미지 url 처리
-                DispatchQueue.global(qos: .default).async(execute: { [self]() -> Void in
-                    
-                    let url = URL(string: plantStepExplainArray[2].imageURL)
-                    let imageData = try? Data(contentsOf: url!)
-                    DispatchQueue.main.async(execute: {() -> Void in
-                        
-                        plantExplainCell.plantImageView.image = UIImage(data: imageData!)
-                    })
-                })
+                if let meaningUrl = URL(string: plantStepExplainArray[2].image) {
+                    setPopUpAsset(plantExplainCell.flowerMeaningImageView, meaningUrl)
+                }
+                if let imageUrl = URL(string: plantStepExplainArray[2].imageURL) {
+                    setPopUpAsset(plantExplainCell.plantImageView, imageUrl)
+                }
             }
         }
-        
         return plantExplainCell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: collectionView.frame.width, height: 283)
     }
-    
-    
 }
 //MARK: - collectionView Horizontal Paging Effect 적용
 extension PlantDetailPopUpExplainVC : UIScrollViewDelegate {
