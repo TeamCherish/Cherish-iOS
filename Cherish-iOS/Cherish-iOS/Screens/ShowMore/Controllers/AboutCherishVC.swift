7b5da89fba032612c8cf3fe18249dd2d69368537
wonseok <wurijang@gmail.com>
2021-12-05 19:33:15 +0900

    [feat] 인스타그램 연결 로직 완성

--- Cherish-iOS/Cherish-iOS/Screens/ShowMore/Controllers/AboutCherishVC.swift
+++ Cherish-iOS/Cherish-iOS/Screens/ShowMore/Controllers/AboutCherishVC.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class AboutCherishVC: BaseController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func icnBackAction(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func goToCherishInsta(_ sender: Any) {
+        
+        let instagramApp = "instagram://user?username=cherish_ofcl"
+        let instagramWeb = "https://www.instagram.com/cherish_ofcl/"
+        guard let instaAppURL = URL.init(string: instagramApp),
+              let instaWebURL = URL.init(string: instagramWeb) else {
+                  self.makeAlert(title: "단장중이에요! 다음에 다시 찾아주세요")
+                  return
+              }
+        if UIApplication.shared.canOpenURL(instaAppURL as URL) {
+            UIApplication.shared.open(instaAppURL as URL)
+        } else {
+            UIApplication.shared.open(instaWebURL as URL)
+        }
     }
 }
