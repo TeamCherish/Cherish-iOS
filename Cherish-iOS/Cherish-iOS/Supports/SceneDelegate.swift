ba72aaec0aa570ee361ee91c739be88ea0eab22b
wonseok <wurijang@gmail.com>
2021-12-07 03:44:46 +0900

    [refactor] SceneDelegate 로직 리팩토링
    - 비밀번호 입력 상황 리팩토링
    - 비밀번호 설정시 앱 InActive에서 블러처리

--- Cherish-iOS/Cherish-iOS/Supports/SceneDelegate.swift
+++ Cherish-iOS/Cherish-iOS/Supports/SceneDelegate.swift
@@ -20,29 +20,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         var initialViewController: UIViewController?
         window = UIWindow(windowScene: windowScene)
         
-        
         // 자동 로그인이 될 때 첫 Scnene을 메인뷰로
         if let _ = UserDefaults.standard.string(forKey: "loginEmail") {
-            // 잠금 설정?
-            if let _ = UserDefaults.standard.value(forKey: "AppLockPW") {
-                let lock = SetLockVC().then {
-                    $0.modeSelect = .input
-                }
-                window?.rootViewController = UINavigationController(rootViewController: lock)
-                window?.makeKeyAndVisible()
-                UIApplication.shared.registerForRemoteNotifications()
+            // 등록된 식물이 하나 이상 존재한다면 메인뷰로 이동
+            if UserDefaults.standard.bool(forKey: "isPlantExist") == true {
+                print("첫 로드 : 메인뷰")
+                let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
+                initialViewController = storyboard.instantiateViewController(withIdentifier: "CherishTabBarController")
             } else {
-                // 등록된 식물이 하나 이상 존재한다면 메인뷰로 이동
-                if UserDefaults.standard.bool(forKey: "isPlantExist") == true {
-                    print("첫 로드 : 메인뷰")
-                    let storyboard = UIStoryboard(name: "TabBar", bundle: nil)
-                    initialViewController = storyboard.instantiateViewController(withIdentifier: "CherishTabBarController")
-                } else {
-                    print("첫 로드 : 등록된 식물이 없어요 뷰")
-                    let storyBoard = UIStoryboard(name: "AddUser", bundle: nil)
-                    let vc = storyBoard.instantiateViewController(withIdentifier: "AddUserVC")
-                    initialViewController = UINavigationController(rootViewController: vc)
-                }
+                print("첫 로드 : 등록된 식물이 없어요 뷰")
+                let storyBoard = UIStoryboard(name: "AddUser", bundle: nil)
+                let vc = storyBoard.instantiateViewController(withIdentifier: "AddUserVC")
+                initialViewController = UINavigationController(rootViewController: vc)
             }
         } else {
             if UserDefaults.standard.bool(forKey: "OnboardingHaveSeen") == true {
@@ -75,22 +64,46 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        guard let _ = UserDefaults.standard.value(forKey: "AppLockPW") else { return }
+        self.setBlurryView(false)
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
+        guard let _ = UserDefaults.standard.value(forKey: "AppLockPW") else { return }
+        self.setBlurryView(true)
     }
     
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
+        if let _ = UserDefaults.standard.value(forKey: "AppLockPW") {
+            let lock = SetLockVC().then { $0.modeSelect = .unlock }
+            window?.rootViewController?.view.window?.addSubview(lock.view)
+        }
     }
     
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        guard let _ = UserDefaults.standard.value(forKey: "AppLockPW") else { return }
+        self.setBlurryView(true)
+    }
+    
+    
+    private func setBlurryView(_ needToShow: Bool) {
+        let viewTag = -703
+        let blurryView = window?.rootViewController?.view.window?.viewWithTag(viewTag)
+        if needToShow {
+            if blurryView == nil {
+                window?.rootViewController?.view.window?.addSubview(BlurryView(viewTag: viewTag))
+            }
+        } else {
+            guard let blurryView = blurryView else { return }
+            blurryView.removeFromSuperview()
+        }
     }
 }
 
